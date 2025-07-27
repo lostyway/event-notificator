85b0898008113d5f974f5ccb4bdb9985ebec69e5
lostway <hardsheller@gmail.com>
2025-07-27 06:15:09 +0300

    Сделал 3 партиции. Поднятие при падении, поднятие сервисов только после инициализации кафки и БД. При падении лидеры заменяют утерянную реплику. Если упало больше двух реплик --> начинают падать ошибки. (которые можно обработать и остановить приложение, если необходимо по бизнес-логике).
    Далее следует реализовать Transactional Outbox для корректной обработки бизнес-логики

--- src/main/java/com/lostway/eventnotificator/kafka/EventKafkaListener.java
+++ src/main/java/com/lostway/eventnotificator/kafka/EventKafkaListener.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class EventKafkaListener {
 
     @KafkaListener(
-            topics = "event_changes",
+            topics = "event-changes",
             containerFactory = "eventKafkaListenerContainerFactory"
     )
     public void listen(EventChangeKafkaMessage message) {
